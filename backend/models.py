d365426aaac44b4c3434c495edd0fb2b4a680f77
akib-siddique22 <akibs2001@gmail.com>
2024-09-15 03:22:04 +0800

    made movie model

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Movies(db.Model):
+    MovieID = db.Column(db.Integer, primary_key = True)
+    Titles = db.Column(db.String(100), nullable = False)
+    Genres = db.Column(db.String(100),nullable = False)
+    tmdbID = db.Column(db.Integer)
+
+    def to_json(self):
+        return {
+            "MovieID": self.MovieID,
+            "Titles": self.Titles,
+            "Genres": self.Genres,
+            "tmdbID": self.tmdbID,
+        }
