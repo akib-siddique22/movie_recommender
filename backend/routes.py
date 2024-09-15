daa761c7e9bf60de98715f35a3e9bc16886e3ab5
akib-siddique22 <akibs2001@gmail.com>
2024-09-16 02:27:41 +0800

    routes to create movie

--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,31 @@
 from app import app, db
 from flask import request, jsonify
-from models import Movies
+from models import Movie
 
 #Get all movies
 @app.route('/api/movies', methods=["GET"])
 def get_movies():
-    movies = Movies.query.all()
+    movies = Movie.query.all()
     result = [movie.to_json() for movie in movies]
     return jsonify(result)
+
+@app.route("/api/movies", methods = ["POST"])
+def create_movies():
+    try:
+        data = request.json
+        
+        MovieID = data.get("MovieID")
+        Title = data.get("Title")
+        Genres = data.get("Genres")
+        tmdbID = data.get("tmdbID")
+
+        new_movie = Movie(MovieID=MovieID, Title=Title, Genres=Genres, tmdbID=tmdbID)
+
+        db.session.add(new_movie)
+        
+        db.session.commit()
+
+        return jsonify({"msg":"Movie created successfully"}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":str(e)}), 500
