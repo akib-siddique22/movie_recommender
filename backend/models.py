13a78a43825571de9734ab9a127c98361b544a8d
akib-siddique22 <akibs2001@gmail.com>
2024-09-16 02:00:40 +0800

    correction to model.py

--- backend/models.py
+++ backend/models.py
@@ -2,14 +2,14 @@ from app import db
 
 class Movie(db.Model):
     MovieID = db.Column(db.Integer, primary_key = True)
-    Titles = db.Column(db.String(100), nullable = False)
+    Title = db.Column(db.String(100), nullable = False)
     Genres = db.Column(db.String(100),nullable = False)
     tmdbID = db.Column(db.Integer)
 
     def to_json(self):
         return {
             "MovieID": self.MovieID,
-            "Titles": self.Titles,
+            "Title": self.Title,
             "Genres": self.Genres,
             "tmdbID": self.tmdbID,
         }
