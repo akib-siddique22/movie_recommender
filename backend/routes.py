4439bef34a09ed949ad7d9bffaf3de7d4a2554c3
akib-siddique22 <akibs2001@gmail.com>
2024-09-18 23:57:51 +0800

    can delete movies, added comments

--- backend/routes.py
+++ backend/routes.py
@@ -9,6 +9,7 @@ def get_movies():
     result = [movie.to_json() for movie in movies]
     return jsonify(result)
 
+#Add a movie
 @app.route("/api/movies", methods = ["POST"])
 def create_movies():
     try:
@@ -34,3 +35,19 @@ def create_movies():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error":str(e)}), 500
+
+#Delete a movie
+@app.route("/api/movies/<int:id>", methods=["DELETE"])
+def delete_movie(id):
+    try:
+        movie = Movie.query.get(id)
+        if movie is None:
+            return jsonify({"error":"Movie not found"}), 404
+        
+        db.session.delete(movie)
+        db.session.commit()
+        return jsonify({"msg": "Movie deleted"}), 200
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
