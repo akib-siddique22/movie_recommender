e1ecee4dc8929f71292924c4520e64548c7b02eb
akib-siddique22 <akibs2001@gmail.com>
2024-09-20 01:43:13 +0800

    basic frontend with react and vite using chakra

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button>hi</Button>
+    </>
+  )
+}
+
+export default App
