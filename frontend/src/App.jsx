e75f26d7bfd72f8acca4e0e8dce00a16d6f92642
akib-siddique22 <akibs2001@gmail.com>
2024-09-27 03:04:33 +0800

    configured the routes of pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import { Button } from '@chakra-ui/react'
 import { useState } from 'react'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import coldStart from './pages/coldStart';
+import hybridFilter from './pages/hybridFilter';
+import userInput from './pages/userInput';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,8 +15,17 @@ function App() {
   return (
     <>
       <Button>hi</Button>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movies/input' element={<userInput />} />
+      <Route path='/movies/cold-start' element={<coldStart />} />
+      <Route path='/movies/hybrid-filtering' element={<hybridFilter />} />
+      </Routes>
     </>
   )
 }
 
 export default App
+
+
+
