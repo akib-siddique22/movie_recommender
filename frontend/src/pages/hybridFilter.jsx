8e791d575f03789b72ead8e47d963c00efb5bac6
akib-siddique22 <akibs2001@gmail.com>
2024-10-06 05:12:40 +0800

    can get hybrid filtering recommendations and 5 titles are displayed

--- frontend/src/pages/hybridFilter.jsx
+++ frontend/src/pages/hybridFilter.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const hybridFilter = () => {
-  return (
-    <div>hybridFilter</div>
-  )
-}
+const HybridFilter = () => {
+    const [recommendations, setRecommendations] = useState([]);
 
-export default hybridFilter
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/final-recommendations');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setRecommendations(data.final_recommendations);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Final Recommendations</h1>
+            <ul>
+                {recommendations.map((recommendation, index) => (
+                    <li key={index}>{recommendation.Title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default HybridFilter;
