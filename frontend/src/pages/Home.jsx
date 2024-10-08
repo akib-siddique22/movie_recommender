6f7d1a1149ddfb50d46de30c796fb4334c0e2f1f
akib-siddique22 <akibs2001@gmail.com>
2024-10-09 03:28:21 +0800

    added simple button to the recommendation system

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button } from "@chakra-ui/react";
 
 const Home = () => {
+  const toInput = () => {
+    window.location.href = "/movies/input";
+  };
+
   return (
-    <div>Home</div>
-  )
+    <Button onClick={toInput}>
+      Recommendation System
+    </Button>
+  );
 }
 
 export default Home
